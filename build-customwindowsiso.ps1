3567ed9f0e2e821f6d2c8c3ea8346b1e6de45444
Luan Vitor Simião Oliveira <luanv.oliveira@outlook.com>
2021-06-27 15:29:19 -0300

    add custom windows iso script

---
+++ build-customwindowsiso.ps1
@@ -0,0 +1,45 @@
+[CmdletBinding()]
+param (
+	[Parameter()]
+	[string]
+	$SourceIsoPath
+)
+
+$ErrorActionPreference = 'Stop'
+
+# https://docs.microsoft.com/en-us/windows-hardware/get-started/adk-install
+Get-Command oscdimg.exe, 7z.exe -ErrorAction Stop
+Test-Path ~\Downloads\appraiserres.dll
+
+$SourceIsoPath = Resolve-Path $SourceIsoPath
+
+$SourceIsoPath
+Push-Location temp:
+
+& {
+	Export-WindowsDriver -Online -Destination .\drivers\
+	Push-Location .\drivers
+	remove-item -Recurse -Force .\arduino*, .\gameflt*, .\vigembus*, .\prnms*, .\xvdd*, .\genuino*, .\gameflt*, .\linino*, .\adafruit*
+	Pop-Location
+}
+
+# https://stackoverflow.com/questions/56869181/how-to-extract-iso-file-with-powershell-2-0
+7z.exe x -y -o".\expandediso" "$SourceIsoPath"
+
+mkdir -Path .\image
+Mount-WindowsImage -Path .\image -ImagePath ".\expandediso\sources\install.wim" -Index 4
+Set-WindowsProductKey -ProductKey "" -Path .\image
+Add-WindowsDriver -Path .\image -Driver .\drivers\ -Recurse
+Enable-WindowsOptionalFeature -Path .\image -FeatureName "Containers", "Microsoft-Hyper-V-All", "Microsoft-Windows-Subsystem-Linux", "Containers-DisposableClientVM", "HypervisorPlatform", "VirtualMachinePlatform", "HostGuardian"
+foreach ($item in "Language.Basic~~~en-US~", "Language.Basic~~~pt-BR~", "Language.Fonts.Jpan~~~und-JPAN~", "Language.Handwriting~~~en-US~", "Language.Handwriting~~~pt-BR~", "Language.OCR~~~en-US~", "Language.OCR~~~pt-BR~", "Language.Speech~~~en-US~", "Language.Speech~~~pt-BR~", "Language.TextToSpeech~~~en-US~", "Language.TextToSpeech~~~pt-BR~", "MathRecognizer~~~~", "OpenSSH.Client~~~~") {
+	Add-WindowsCapability -Path .\image -Name $item
+}
+dismount-WindowsImage -Path .\image -Save -CheckIntegrity
+
+Remove-Item -Path .\expandediso\sources\appraiserres.dll -Force
+Copy-Item -Path ~\Downloads\appraiserres.dll -Destination .\expandediso\sources\
+
+# https://winaero.com/how-to-install-windows-11-without-tpm-2-0/
+cmd /c "oscdimg -m -o -u2 -udfver102 -bootdata:2#p0,e,b$(Convert-Path .\expandediso\boot\etfsboot.com)#pEF,e,b$(Convert-Path .\expandediso\efi\Microsoft\boot\efisys.bin) $(Convert-Path .\expandediso) $(Convert-Path ~\)\CustomWindows.iso"
+
+Pop-Location
